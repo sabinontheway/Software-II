e5cfbc7a68475ff8827049604a1e2f85040d6deb
pranjalghimire <51348145+pranjalghimire@users.noreply.github.com>
2020-02-13 15:53:11 -0600

    Codes are fixed

--- Software.java
+++ Software.java
@@ -106,5 +106,141 @@ public class Software
 			customer.printInformation();
 		}
 	}
+	// method to read one account
+	public void readOneAccountOfCustomer(Scanner scanner)
+	{
+		int type = BankService.getCustomerLoginInput(scanner);
+		if( type == 0 )
+			return;
+		
+		BankCustomer customer = null;
+		if( type == 1 )
+		{
+			System.out.println("Enter id : ");
+			String id = scanner.nextLine();
+			
+			// check if the customer is found
+			customer = getCustomerById(id);
+			// if this is null
+			if( customer == null )
+			{
+				// show message customer not found
+				System.out.println("Customer with id " + id + " does not exist");
+				return;
+			}
+		}
+		else if ( type == 2 )
+		{
+			System.out.println("Enter username : ");
+			String username = scanner.nextLine();
+			System.out.println("Enter password : ");
+			String password = scanner.nextLine();
+			
+			// check if the customer is found
+			customer = getCustomerByUserPassword(username, password);
+			// if this is null
+			if( customer == null )
+			{
+				// show message customer not found
+				System.out.println("Customer with username " + username + " and password " + password + " does not exist");
+				return;
+			}
+		}
+		
+		System.out.print("Enter account number : ");
+		// read account number
+		String accountNumber = scanner.nextLine();
+		
+		if( customer.findAccount(accountNumber) == null )
+		{
+			System.out.println("Account with number " + accountNumber + " not found!");
+			return;
+		}
+		else
+		{
+			Account account = customer.findAccount(accountNumber);
+			System.out.println("Account type    : "+ account.getType() + " Account");
+			System.out.println("Account number  : "+ account.getNumber() );
+			System.out.println("Account balance : $"+ account.getBalance() );
+			
+			// get the type
+			if( account.getType().equals("Checking"))
+			{
+				// print the type
+				
+				// get the checking
+				CheckingAccount checking = (CheckingAccount)account;
+				// show limit and service fee
+				System.out.println("Limit Amount    : $"+ checking.getLimit());
+				System.out.println("Service fee     : $"+ checking.getServiceFee() );
+			}
+			else
+			{
+				// get savings
+				SavingAccount saving = (SavingAccount)account;
+				// show limit and service fee
+				System.out.println("Limit Amount    : $"+ saving.getLimit() );
+				System.out.println("Interest rate   : "+ saving.getInterestRate() + "%");
+			}
+		}
+	}
+	
+	// method to open new account
+	public void openNewAccount(Scanner scanner, BankCustomer customer)
+	{
+		String id = "";
+		if( customer == null )
+		{
+			System.out.println("Enter id : ");
+			id = scanner.nextLine();
+			
+			// check if the customer is found
+			customer = getCustomerById(id);
+			// if this is null
+			if( customer == null )
+			{
+				// show message customer not found
+				System.out.println("Customer with id " + id + " does not exist");
+				return;
+			}
+		}
+		
+		// Ask for account choice
+		System.out.println("Enter Account type choice\n1.Checking\n2.Savings");
+		// ask choice
+		int accountChoice = Integer.parseInt(scanner.nextLine());
+		
+		// generate number
+		String accountNumber = BankService.getNewAccountNumber(this);
+		// get balance
+		System.out.print("Enter starting balance : ");
+		float balance = Float.parseFloat(scanner.nextLine());
+		
+		// if this is check
+		if( accountChoice == 1 ) 
+		{
+			
+			// ask service fee
+			System.out.print("Enter service fee : ");
+			float serviceFee = Float.parseFloat(scanner.nextLine());
+			
+			// add new account to this customer
+			customer.addAccount( new CheckingAccount(accountNumber, balance, limit, serviceFee));
+		}
+		else
+		{
+		
+			// ask service fee
+			System.out.print("Enter interest rate : ");
+			float interestRate = Float.parseFloat(scanner.nextLine());	
+			
+			// create new account
+			customer.addAccount( new SavingAccount(accountNumber, balance, limit, interestRate));
+		}
+		
+		// show success
+		System.out.println("Open new Account for Customer with id: " + id + " SUCCESS");
+		
+	}
 	
 	
