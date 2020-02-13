0fbf2f01cd04d7241083cc5880c561bf54f0ba2e
bharatpokharel <49163932+bharatpokharel@users.noreply.github.com>
2020-02-12 20:36:48 -0600

    Update BankCustomer.java

--- BankCustomer.java
+++ BankCustomer.java
@@ -28,3 +28,93 @@ public class BankCustomer
 		// create acc list
 		acc = new LinkedList<>();
 	}
+// to open a nee account
+	public void addAccount(Account account)
+	{
+		acc.add(account);
+	}
+	
+	
+	public String csvString()
+	{
+		
+		String str = id + "," +
+				firstName + "," + 
+				lastName + "," +  
+				userName + "," +  
+				password + "," + 
+				address;
+		
+	
+		for( Account account : acc )
+		{
+			// add the csv string to it
+			str = str + "," + account.toString();
+		}
+		
+		// return the return string
+		return str;
+	}
+	
+	
+	public void customerInfo()
+	{
+		System.out.println("Customer Name     : " + lastName + ", " + firstName);
+		System.out.println("Customer id       : " + id);
+		System.out.println("Address           : " + address);
+	}
+	
+	// show account details
+	public void accDetails()
+	{
+		System.out.println("List of accounts: ");
+		// for each account
+		for( Account account : acc )
+		{
+			// account types
+			System.out.println("Account type    : "+ account.getType() + " Account");
+			System.out.println("Account number  : "+ account.getNumber() );
+			System.out.println("Account balance : $"+ account.getBalance() );
+			
+			// for checking accounts
+			if( account.getType().equals("Checking"))
+			{
+				
+				CheckingAccount checking = (CheckingAccount)account;
+				// show limit and service fee
+				System.out.println("Limit Amount    : $"+ checking.getLimit());
+				System.out.println("Service fee     : $"+ checking.getServiceFee() );
+			}
+			else
+			{
+				//for savings account
+				SavingAccount saving = (SavingAccount)account;
+				// show limit and service fee
+				System.out.println("Limit Amount    : $"+ saving.getLimit() );
+				System.out.println("Interest rate   : "+ saving.getInterestRate() + "%");
+			}
+		}
+	}
+	
+	// for customer information
+	public void printInformation()
+	{
+		customerInfo();
+		System.out.println();
+		accDetails();
+		System.out.println();
+	}
+	
+	
+	public Account findAccount(String accountNumber)
+	{
+		for( Account account : acc )
+		{
+			
+			if( account.getNumber().equals(accountNumber))
+			{
+				return account;
+			}
+		}
+		return null;
+	}
