e999b6f68fc5ad701a2c07522ff11ab048d60a08
sabinontheway <49572827+sabinontheway@users.noreply.github.com>
2020-04-03 18:16:57 -0500

    BankCustomerTest  updated

--- test/functional/JUnit/BankCustomerTest.java
+++ test/functional/JUnit/BankCustomerTest.java
@@ -45,5 +45,36 @@ public class BankCustomerTest {
         if(expResult != result)
         fail("The test case is a prototype.");
     }
-}
     
+    /**
+     * Test of getPassword method, of class BankCustomer.
+     */
+    @Test
+    public void testGetPassword() {
+        System.out.println("getPassword");
+        BankCustomer instance = new BankCustomer("100","Sabin", "Poudel","poud","poud","Texas");
+        String expResult = "poud";
+        String result = instance.getPassword();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        if(expResult != result)
+        fail("The test case is a prototype.");
+    }
+    
+        /**
+     * Test of getNumAccounts method, of class BankCustomer.
+     */
+    @Test
+    public void testGetNumAccounts() {
+        System.out.println("getNumAccounts");
+        BankCustomer instance = new BankCustomer("100","Sabin", "Poudel","poud","poud","Texas");
+        int expResult = 0;
+        int result = instance.getNumAccounts();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        if(expResult != result)
+        fail("The test case is a prototype.");
+    }
+    
+}
+
